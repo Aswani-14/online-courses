b17347439f276ff4482970263348fcf36c0de834
Aswani K B <aswanikb14@gmail.com>
2026-02-28 14:34:22 +0530

    Register User model in admin site

--- admin.py
+++ admin.py
@@ -1,33 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-
 from .models import Course, Lesson, Enrollment, Question, Choice, Submission
 
-
-# Inline for Choice inside Question
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 2
 
-
-# Inline for Question inside Course
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 1
 
-
-# Question Admin
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ('question_text', 'course', 'grade')
 
-
-# Lesson Admin
 class LessonAdmin(admin.ModelAdmin):
     list_display = ('title', 'course', 'order')
 
-
-# Register models
 admin.site.register(Course)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Enrollment)
