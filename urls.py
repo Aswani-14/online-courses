d73f139983ced7b720007d74c854ef99dff027cb
Aswani K B <aswanikb14@gmail.com>
2026-02-28 13:57:18 +0530

    Add paths for submitting and displaying exam results

--- urls.py
+++ urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Path to submit the exam
     path('submit/<int:course_id>/', views.submit_exam, name='submit_exam'),
+    
+    # Path to display and evaluate the exam result
     path('result/<int:submission_id>/', views.show_exam_result, name='exam_result'),
 ]
